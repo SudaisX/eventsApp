5c76e92ccc7d599af0561840d41a726d9374f958
Sudais <44402465+sudaisx@users.noreply.github.com>
2022-07-31 13:34:29 +0500

    delete functionality added

--- frontend/pages/events/[slug].js
+++ frontend/pages/events/[slug].js
@@ -1,13 +1,30 @@
 import { FaPencilAlt, FaTimes } from 'react-icons/fa';
 import Link from 'next/link';
 import Image from 'next/image';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Layout from '@/components/Layout';
 import { API_URL } from '@/config/index';
 import styles from '@/styles/Event.module.css';
+import { useRouter } from 'next/router';
 
 export default function EventPage({ evt }) {
-    const deleteEvent = (e) => {
-        console.log('delete');
+    const router = useRouter();
+
+    const deleteEvent = async (e) => {
+        if (confirm('Are you sure?')) {
+            const res = await fetch(`${API_URL}/api/events/${evt.id}`, {
+                method: 'DELETE',
+            });
+
+            const data = await res.json();
+
+            if (!res.ok) {
+                return toast.error(data.message);
+            } else {
+                router.push('/events');
+            }
+        }
     };
 
     return (
@@ -17,44 +34,52 @@ export default function EventPage({ evt }) {
                     <a className={styles.back}> {'<'} Go Back</a>
                 </Link>
 
-                <div className={styles.controls}>
-                    <Link href={`/events/edit/${evt.id}`}>
-                        <a>
-                            <FaPencilAlt /> Edit Event
-                        </a>
-                    </Link>
-                    <a href='#' className={styles.delete} onClick={deleteEvent}>
-                        <FaTimes /> Delete Event
-                    </a>
-                </div>
+                {evt ? (
+                    <>
+                        <div className={styles.controls}>
+                            <Link href={`/events/edit/${evt.id}`}>
+                                <a>
+                                    <FaPencilAlt /> Edit Event
+                                </a>
+                            </Link>
+                            <a href='#' className={styles.delete} onClick={deleteEvent}>
+                                <FaTimes /> Delete Event
+                            </a>
+                        </div>
 
-                <span>
-                    {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
-                </span>
+                        <span>
+                            {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
+                        </span>
 
-                <h1>{evt.name}</h1>
+                        <h1>{evt.name}</h1>
 
-                {evt.image && (
-                    <div className={styles.image}>
-                        <Image
-                            // src={evt.image.data.attributes.url}
-                            src={
-                                evt.image && evt.image.data
-                                    ? evt.image.data.attributes.url
-                                    : '/images/event-default.png'
-                            }
-                            layout='fill'
-                            objectFit='contain'
-                            alt='image'
-                        />
-                    </div>
+                        <ToastContainer />
+
+                        {evt.image && (
+                            <div className={styles.image}>
+                                <Image
+                                    // src={evt.image.data.attributes.url}
+                                    src={
+                                        evt.image && evt.image.data
+                                            ? evt.image.data.attributes.url
+                                            : '/images/event-default.png'
+                                    }
+                                    layout='fill'
+                                    objectFit='contain'
+                                    alt='image'
+                                />
+                            </div>
+                        )}
+
+                        <h3>Description</h3>
+                        <p>{evt.description}</p>
+
+                        <h3>Venue: {evt.venue}</h3>
+                        <p>{evt.address}</p>
+                    </>
+                ) : (
+                    <h1>Event not found..</h1>
                 )}
-
-                <h3>Description</h3>
-                <p>{evt.description}</p>
-
-                <h3>Venue: {evt.venue}</h3>
-                <p>{evt.address}</p>
             </div>
         </Layout>
     );
@@ -90,7 +115,7 @@ export async function getServerSideProps({ query: { slug } }) {
     const events = data.map((evt) => ({ id: evt.id, ...evt.attributes }));
     return {
         props: {
-            evt: events[0],
+            evt: events[0] ? events[0] : null,
         },
     };
 }
